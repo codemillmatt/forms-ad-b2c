0be8c0888fd3273c03f861821a581b2619a6976a
Matt Soucoup <msoucoup@codemilltech.com>
2017-12-15 09:27:21 -0600

    Adding login service as separate solution

---
+++ 4-login-service/forms/iOS/AppDelegate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.Identity.Client;
+
+using Foundation;
+using UIKit;
+
+using TheReviewer.Core;
+
+namespace TheReviewer.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+
+        public override bool OpenUrl(UIApplication app, NSUrl url, NSDictionary options)
+        {
+            AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(url);
+
+            return true;
+        }
+    }
+}
