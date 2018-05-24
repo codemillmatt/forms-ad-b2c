811980de761d92f2e7346d70bea09bb5ffb2a521
Matt Soucoup <msoucoup@codemilltech.com>
2018-05-24 07:59:11 -0500

    CSS stuff

--- 4-login-service/forms/TheReviewer.Core/App.xaml.cs
+++ 4-login-service/forms/TheReviewer.Core/App.xaml.cs
@@ -9,29 +9,9 @@ namespace TheReviewer.Core
 {
     public partial class App : Application
     {
-        public static string Tenant = "TheReviewer.onmicrosoft.com";
-        public static string ClientID = "978f6a35-db30-44fd-8544-b7cc40466adc";
-        public static string SignUpAndInPolicy = "B2C_1_GenericSignUpAndIn";
-
-        public static string[] Scopes = new string[] { "https://asdf.com/rvw.read.only" };
-        public static string ApiEndpoint = "http://thereviewer.azurewebsites.net/api/reviews";
-
-        public static string AuthorityBase = $"https://login.microsoftonline.com/tfp/{Tenant}/";
-        public static string Authority = $"{AuthorityBase}{SignUpAndInPolicy}";
-        //public static string AuthorityEditProfile = $"{AuthorityBase}{PolicyEditProfile}";
-        //public static string AuthorityPasswordReset = $"{AuthorityBase}{PolicyResetPassword}";
-
-        public static PublicClientApplication AuthClient = null;
-
-        public static UIParent UiParent = null;
-
         public App()
         {
             InitializeComponent();
-
-            AuthClient = new PublicClientApplication(ClientID, Authority);
-            AuthClient.ValidateAuthority = false;
-            AuthClient.RedirectUri = $"msal{ClientID}://auth";
 
             MainPage = new NavigationPage(new ReviewListPage());
         }
